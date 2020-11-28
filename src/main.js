b1b82eacf4542c36fb0cdd547fc7479253e9c5af
tootofu <tofudeliveryboi@outlook.com>
2020-11-28 18:37:36 -0400

    Fix url check

--- src/main.js
+++ src/main.js
@@ -1,31 +1,30 @@
 'use strict';
 
-import UI from './services/commons/ui'
-// Hispachan/Hispasexy imports
-import { 
-  HISPA_THREAD,
-  HISPA_BOARD,
-  HISPA_CATALOG
-} from './services/hispachan/regex'
+import UI from './lib/ui'
 
+import { THREAD, BOARD, CATALOG } from './services/hispachan/regex'
 import { Thread, Board } from './services/hispachan/models'
 
 
-if ( HISPA_THREAD.test( document.URL ) ) {
+if ( THREAD.test( document.URL ) ) {
 
   const threadNode = document.querySelector('.thread');
-  const thread = Thread.fromNode( threadNode );
 
-  UI.renderOnThread( thread );
+  if ( threadNode ) {
+    const thread = Thread.fromNode( threadNode );
+    UI.renderOnThread( thread );
+  }
 
-} else if ( HISPA_BOARD.test( document.URL ) ) {
+} else if ( BOARD.test( document.URL ) ) {
 
   const boardNode = document.getElementById('delform');
-  const board = Board.fromNode( boardNode );
 
-  UI.renderOnBoard( board );
+  if ( boardNode ) {
+    const board = Board.fromNode( boardNode );
+    UI.renderOnBoard( board );
+  }
 
-} else if ( HISPA_CATALOG.test( document.URL ) ) {
+} else if ( CATALOG.test( document.URL ) ) {
 
   // UwU
 
--- src/services/hispachan/regex.js
+++ src/services/hispachan/regex.js
@@ -1,5 +1,5 @@
 'use strict';
 
-export const HISPA_THREAD   =  new RegExp('https:\/\/www\.(hispachan|hispasexy)\.org\/[a-z]*\/res\/[0-9]*\.html$');
-export const HISPA_BOARD    =  new RegExp('https:\/\/www\.(hispachan|hispasexy)\.org\/[a-z]*\/$');
-export const HISPA_CATALOG  =  new RegExp('https:\/\/www\.(hispachan|hispasexy)\.org\/[a-z]*\/catalog\.html$');
+export const THREAD   =  new RegExp('https:\/\/www\.(hispachan|hispasexy)\.org\/[a-z]*\/res\/[0-9]*\.html(#i[0-9]*)?$');
+export const BOARD    =  new RegExp('https:\/\/www\.(hispachan|hispasexy)\.org\/[a-z]*\/([0-9]\.html)?$');
+export const CATALOG  =  new RegExp('https:\/\/www\.(hispachan|hispasexy)\.org\/[a-z]*\/catalog\.html$');
